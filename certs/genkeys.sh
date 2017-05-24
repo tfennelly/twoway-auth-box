02801dd7f04230c821575f228653dfad0abdab65
tfennelly <tom.fennelly@gmail.com>
2017-05-24 10:06:17 +0100

    generate Java keystore and truststore for jetty/java server

--- certs/genkeys.sh
+++ certs/genkeys.sh
@@ -31,6 +31,11 @@ openssl req -new -key server.key -out server.csr -subj "$SUBJECT_BASE-server"
 # We're self signing our own server cert here.  This is a no-no in production.
 openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
+# Java KeyStore for the server. Use in the jetty-server. Note that the nginx proxy
+# uses the raw .key and .crt pem files.
+openssl pkcs12 -export -out server.p12 -inkey server.key -in server.crt
+keytool -importkeystore -destkeystore server.keystore -srckeystore server.p12 -srcstoretype PKCS12 -noprompt
+
 # Create the Client Key and CSR
 openssl genrsa -des3 -out client.key 1024 -subj "$SUBJECT_BASE-client"
 openssl rsa -in client.key -out client.key
@@ -39,6 +44,12 @@ openssl req -new -key client.key -out client.csr -subj "$SUBJECT_BASE-client"
 # Sign the client certificate with our CA cert.  Unlike signing our own server cert, this is what we want to do.
 openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
 
+# Java KeyStore for the client. Not actually used by the java-client project,
+# which constructs the keystore and truststore manually itself. It does this because often times
+# java clients have acess to the client key and cert, but are not in a position to get at a pre-generated
+# keystore and truststore.
 openssl pkcs12 -export -out client.p12 -inkey client.key -in client.crt
 keytool -importkeystore -destkeystore client.keystore -srckeystore client.p12 -srcstoretype PKCS12 -noprompt
-keytool -import -v -trustcacerts -keystore client.truststore -noprompt -alias cacert -file ca.crt -noprompt
+
+# Note that the server and client can use the same truststore i.e. from the same shared ca.crt.
+keytool -import -v -trustcacerts -keystore truststore -noprompt -alias cacert -file ca.crt -noprompt
